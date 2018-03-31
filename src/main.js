4dc09593ce6d89978b88989a66f355ba2f836698
sairoutine <sairoutine@gmail.com>
2018-03-31 17:35:40 +0900

    update

---
+++ src/main.js
@@ -0,0 +1,14 @@
+'use strict';
+const canvas = document.getElementById('canvas-webgl');
+
+const on = () => {
+canvas.addEventListener('contextmenu', function (event) {
+  event.preventDefault();
+});
+canvas.addEventListener('selectstart', function (event) {
+  event.preventDefault();
+});
+}
+
+
+require('./init/glitch.js').default();
